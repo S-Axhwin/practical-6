a6297176f784a8e81313c53a8706a555890950bb
S-Axhwin <ashwin.sathiyamoorthy.d@gmail.com>
2025-04-04 11:23:44 +0530

    multi thread

--- index.c
+++ index.c
@@ -12,11 +12,19 @@ void *show(void *msg) {
   printf("%d\n", mul);
 }
 
+void* evenOdd(void *msg) {
+  printf((*(int *)msg)%2==0?"even\n":"odd\n");
+}
+
 void main() {
   pthread_t t1;
   int n;
   scanf("%d", &n);
   pthread_create(&t1, NULL, show,&n);
   pthread_join(t1, NULL);
+
+  pthread_t t2;
+  pthread_create(&t2, NULL, evenOdd, &n);
+  pthread_join(t2, NULL);
   printf("Bye Bye!\n");
 }
